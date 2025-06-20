e6e8d7898c97fa9b607725241917e12fc7685694
202504_kimsg <tomato.202504.kimsg@gmail.com>
2025-06-20 17:30:03 +0900

    4장 디버그 완료

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
+
+import { AppModule } from './app/app.module';
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
