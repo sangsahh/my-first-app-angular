8ec819b41ce3e5b78824750bae838a0c4eb7d843
202504_kimsg <tomato.202504.kimsg@gmail.com>
2025-06-20 11:38:01 +0900

    45번 테스트 성공

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,52 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  styles : [`
+    .white{
+      color : white;
+    }
+
+    .content5
+    {
+      color : white;
+    }
+    `
+  ]
 })
 export class App {
   protected title = 'my-first-app';
+
+  showSecret = false;
+  logs = [];
+  logItem = 0;
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(this.logs.length + 1);
+  }
+
+
+
+
+
+  contentCreated = false;
+
+  contents = [];
+
+  index = 0;
+
+
+  onContent(){
+    this.contentCreated = true;
+    this.index++;
+    this.contents.push(this.index);
+  }
+
+  getColor(content) {
+    return content > 4 ? 'blue' : '';
+  }
+
+  getClass(content){
+    return content > 4 ? 'white' : '';
+  }
 }
